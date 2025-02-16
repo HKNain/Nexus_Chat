f4fbd83c6abac21cab8776b5522507d623dc64d2
Hitesh <hiteshkumarnain@gmail.com>
2025-02-16 17:14:22 +0530

    ui completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Home from './pages/home/Home'
 
 const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='p-4 h-screen flex items-center justify-center'>
+      <Home />
+    </div>
   )
 }
 
