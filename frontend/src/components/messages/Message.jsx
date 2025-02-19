7bc04914e6d2e7a78403ef32bb1046b7249205af
Hitesh <hiteshkumarnain@gmail.com>
2025-02-19 19:20:18 +0530

    completed

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -13,6 +13,7 @@ const Message = ({ message }) => {
     ? authUser.profilePic
     : selectedConversation?.profilePic;
   const bubblebgColor = fromMe ? "bg-blue-500" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
   return (
     <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
@@ -20,7 +21,9 @@ const Message = ({ message }) => {
           <img src={profilePic} alt="tailwindcss chat bubble component" />
         </div>
       </div>
-      <div className={`chat-bubble text-white ${bubblebgColor} pb-2`}>
+      <div
+        className={`chat-bubble text-white ${bubblebgColor} ${shakeClass} pb-2`}
+      >
         {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
